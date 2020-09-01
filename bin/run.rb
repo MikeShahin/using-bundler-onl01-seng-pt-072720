02933d7f981aa2e19bbff82d02721212eeaa114a
Mike Shahindoust <gobias101@gmail.com>
2020-09-01 01:15:47 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,16 +1,2 @@
-
-Hashie
-Sinatra
-Octokit
-Pry
-AwesomePrint
-
 require_relative '../config/environment'
 
-    source "https://rubygems.org"
-    gem "rspec"
-    gem "sinatra"
-     
-    group :development do
-    gem "pry"
-    end
